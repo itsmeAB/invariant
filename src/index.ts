e0a88a16135222c9b4feb44073b1962c80a299ed
Chris Trevino <darthtrevino@gmail.com>
2019-11-26 08:53:53 -0800

    use code from @darthtrevino branch

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
 /**
- * Copyright (c) 2013-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-'use strict';
-
-/**
  * Use invariant() to assert state which your program assumes to be true.
  *
  * Provide sprintf-style format (only %s is supported) and arguments
@@ -18,7 +9,7 @@
  * will remain to ensure logic does not differ in production.
  */
 
-var invariant = function(condition, format, a, b, c, d, e, f) {
+export default function invariant(condition: any, format: string, a?: any, b?: any, c?: any, d?: any, e?: any, f?: any) {
   if (process.env.NODE_ENV !== 'production') {
     if (format === undefined) {
       throw new Error('invariant requires an error message argument');
@@ -45,5 +36,3 @@ var invariant = function(condition, format, a, b, c, d, e, f) {
     throw error;
   }
 };
-
-module.exports = invariant;
--- test/flow/index.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-var invariant = require('../..');
-
-invariant(true);
